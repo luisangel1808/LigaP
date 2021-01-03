f0aa0b6819375ede1ddbd76e5c248755d3b8adc8
luisangel1808 <luisangel1808@hotmail.com>
2021-01-02 19:16:11 -0500

    Arreglo de lichess.js

--- js/lichess.js
+++ js/lichess.js
@@ -1,32 +1,7 @@
-const fetchData = (url_api) => {
-  return new Promise ((resolve, reject) => {
-      const xhttp = new XMLHttpRequest();
-      xhttp.open('GET', url_api, true);
-      xhttp.onreadystatechange = (() => {
-        if(xhttp.readyState === 4){
-          if(xhttp.status === 200){
-              try {
-                resolve (JSON.parse(xhttp.responseText))
-              } catch (SyntaxError) {
-                resolve (ndJSONtoJSON(xhttp.responseText))
-              }            
-          }
-          else{
-            reject(new Error('Error', url_api))
-          }
-        } 
-      })
-      xhttp.send();
-  })
-}
-
-const ndJSONtoJSON = (ndjson) =>{
-  ndjson = ("[" + ndjson + "]")
-  var enter = /\n/gi;    
-  ndjson = ndjson.replace(enter, ",");
-  var final = /,]/gi;
-  ndjson = ndjson.replace(final, "]");
-  return JSON.parse(ndjson)  
+async function fetchData(url){
+  const response = await fetch(url);
+  const data = await response.json();
+  return data
 }
 
 const API = 'https://lichess.org/api/';
@@ -44,35 +19,15 @@ async function dataUser(url_api) {
   return datos 
 }
 
-async function dataTournament(url_api) {  
-
-  try {
-    tournamentData = await fetchData(`${url_api}tournament/CAw3JUzg/results`);
-    var tournamentDataCauca = []
-    for(let i=0; i<tournamentData.length; i++) {
-      for(let j=0; j<JUGADORES.length; j++) {
-          if(tournamentData[i].username.toLowerCase()===JUGADORES[j]){
-            tournamentDataCauca.push(tournamentData[i])
-          }
-      }
-    }
-  } catch (error) {
-    console.error(error);
-  }  
-  
-  return tournamentDataCauca
-}
-
 async function genera_tabla() {
   const dataPlayer = await dataUser(API);
-  const dataTourney = await dataTournament(API);
   var body = document.getElementsByClassName("main")[0];
   var tabla   = document.createElement("table"); 
   tabla.classList.add("tableLichess") 
   var tblHead = document.createElement("thead");
   tblHead.classList.add("theadL")
   var tblBody = document.createElement("tbody");
-  var titulo  = ['Usuario','','Nombre','Blitz','Bala','Puntos Interligas','Performance Interligas', 'Partidas']
+  var titulo  = ['Usuario','','Nombre','Blitz','Bala', 'Partidas']
   var hilera = document.createElement("tr");
   for (var i = 0; i < titulo.length; i++) {
     var celda = document.createElement("th");
@@ -109,24 +64,6 @@ async function genera_tabla() {
           var textoCelda = document.createTextNode(dataPlayer[i].perfs.bullet.rating);
           break
         case 5:
-          var played= false
-          for (var k=0;k<dataTourney.length;k++){            
-            if(dataPlayer[i].username===dataTourney[k].username){
-              var played= true
-              var textoCelda = document.createTextNode(dataTourney[k].score);
-              break
-            }
-          }
-          if(!played){
-            var textoCelda = document.createTextNode("No participó"); 
-          }                  
-          break
-        case 6:
-          (played)
-          ? textoCelda = document.createTextNode(dataTourney[k].performance)
-          : textoCelda = document.createTextNode("No participó");                
-          break
-        case 7:
           var URL=`https://lichess.org/api/games/user/${dataPlayer[i].username}`
           var textoCelda = document.createElement("button")
           textoCelda.classList.add("button")
